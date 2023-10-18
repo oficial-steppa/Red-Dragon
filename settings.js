6aea9a0c215bc9b510371b684f56f923249953a4
yng_steppa²⁵⁴ <140620922+oficial-steppa@users.noreply.github.com>
2023-10-18 05:38:49 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -56,7 +56,7 @@ global.dragonemoji = '🧩' // Menu emoji
 global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
 
 //database
-global.premium = ['+380944814219'] //ur premium numbers
+global.premium = ['+254790816481'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
